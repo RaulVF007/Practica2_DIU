b531b667a0f906a310d49f36f44f38929a8ce6a8
RaulVF007 <raulvf007@gmail.com>
2022-11-03 14:42:10 +0000

    Final version: using buttons, jLabels and jTextFields components

--- src/main/java/UI/CurrencyConverter.java
+++ src/main/java/UI/CurrencyConverter.java
@@ -29,47 +29,23 @@ public class CurrencyConverter extends javax.swing.JFrame {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        fromComboBox = new javax.swing.JComboBox<>();
-        toComboBox = new javax.swing.JComboBox<>();
         jLabel4 = new javax.swing.JLabel();
         convertButton = new javax.swing.JButton();
         euroDollarConversion = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
         resultConverter = new javax.swing.JLabel();
         exitButton = new javax.swing.JButton();
-        txtConvert = new javax.swing.JTextField();
+        USDtoEUR = new javax.swing.JTextField();
+        EURtoUSD = new javax.swing.JTextField();
+        jLabel6 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setText("CurrencyConverter");
 
-        jLabel2.setText("From:");
-
-        jLabel3.setText("To:");
-
-        fromComboBox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Dollars\t", "Euros" }));
-        fromComboBox.addItemListener(new java.awt.event.ItemListener() {
-            public void itemStateChanged(java.awt.event.ItemEvent evt) {
-                fromComboBoxItemStateChanged(evt);
-            }
-        });
-        fromComboBox.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                fromComboBoxActionPerformed(evt);
-            }
-        });
-
-        toComboBox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Euros", "Dollars" }));
-        toComboBox.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                toComboBoxActionPerformed(evt);
-            }
-        });
-
-        jLabel4.setText("Amount to convert:");
+        jLabel4.setText("Convert from USD ($) to EUR (€): ");
 
         convertButton.setText("Convert");
         convertButton.addActionListener(new java.awt.event.ActionListener() {
@@ -92,152 +68,130 @@ public class CurrencyConverter extends javax.swing.JFrame {
             }
         });
 
-        txtConvert.addActionListener(new java.awt.event.ActionListener() {
+        USDtoEUR.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                txtConvertActionPerformed(evt);
+                USDtoEURActionPerformed(evt);
             }
         });
+
+        EURtoUSD.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                EURtoUSDActionPerformed(evt);
+            }
+        });
+
+        jLabel6.setText("Convert from EUR (€) to USD ($): ");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(87, 87, 87)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(137, 137, 137)
-                                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addGap(80, 80, 80))
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addGap(18, 18, 18)
-                                .addComponent(fromComboBox, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addGap(172, 172, 172)
-                                .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(toComboBox, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addGap(273, 273, 273)))
-                        .addGap(22, 22, 22))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(119, 119, 119)
-                        .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 367, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(116, 116, 116))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(132, 132, 132)
+                .addGap(180, 180, 180)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGap(377, 377, 377)
-                        .addComponent(exitButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addGap(42, 42, 42))
+                        .addComponent(convertButton, javax.swing.GroupLayout.PREFERRED_SIZE, 212, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(235, 235, 235))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                             .addGroup(layout.createSequentialGroup()
-                                .addGap(152, 152, 152)
-                                .addComponent(convertButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(127, 127, 127)
-                                .addComponent(txtConvert)))
-                        .addGap(243, 243, 243))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(resultConverter, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addGap(82, 82, 82))))
+                                .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addGap(48, 48, 48)
+                                .addComponent(USDtoEUR, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(EURtoUSD, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(170, 170, 170))))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(119, 119, 119)
+                .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 367, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(euroDollarConversion)
-                .addGap(269, 269, 269))
+                .addComponent(euroDollarConversion, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(300, 300, 300))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(249, 249, 249))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(exitButton, javax.swing.GroupLayout.PREFERRED_SIZE, 84, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(54, 54, 54))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(resultConverter, javax.swing.GroupLayout.PREFERRED_SIZE, 334, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(179, 179, 179)
+                    .addComponent(jLabel6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGap(319, 319, 319)))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(22, 22, 22)
-                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 32, Short.MAX_VALUE)
-                .addGap(26, 26, 26)
-                .addComponent(euroDollarConversion, javax.swing.GroupLayout.PREFERRED_SIZE, 16, Short.MAX_VALUE)
-                .addGap(21, 21, 21)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(1, 1, 1)
-                        .addComponent(fromComboBox, javax.swing.GroupLayout.DEFAULT_SIZE, 26, Short.MAX_VALUE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(6, 6, 6)
-                        .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, 21, Short.MAX_VALUE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(1, 1, 1)
-                        .addComponent(toComboBox))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(6, 6, 6)
-                        .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, 21, Short.MAX_VALUE)))
-                .addGap(42, 42, 42)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(2, 2, 2)
-                        .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, 24, Short.MAX_VALUE))
-                    .addComponent(txtConvert, javax.swing.GroupLayout.DEFAULT_SIZE, 26, Short.MAX_VALUE))
-                .addGap(36, 36, 36)
-                .addComponent(convertButton, javax.swing.GroupLayout.PREFERRED_SIZE, 23, Short.MAX_VALUE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(29, 29, 29)
+                .addComponent(euroDollarConversion)
+                .addGap(46, 46, 46)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(USDtoEUR, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, 24, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addComponent(EURtoUSD, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(33, 33, 33)
-                .addComponent(resultConverter, javax.swing.GroupLayout.DEFAULT_SIZE, 21, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jLabel5)
-                        .addGap(60, 60, 60))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGap(21, 21, 21)
-                        .addComponent(exitButton, javax.swing.GroupLayout.DEFAULT_SIZE, 30, Short.MAX_VALUE)
-                        .addGap(13, 13, 13)))
-                .addGap(1, 1, 1))
+                .addComponent(convertButton)
+                .addGap(35, 35, 35)
+                .addComponent(resultConverter, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel5)
+                .addGap(18, 18, 18)
+                .addComponent(exitButton)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(191, 191, 191)
+                    .addComponent(jLabel6, javax.swing.GroupLayout.DEFAULT_SIZE, 24, Short.MAX_VALUE)
+                    .addGap(168, 168, 168)))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void fromComboBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_fromComboBoxActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_fromComboBoxActionPerformed
-
     private void convertButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_convertButtonActionPerformed
-        double amountToConvertion = Double.parseDouble(txtConvert.getText());
-        if(amountToConvertion > 0){
-            if(fromComboBox.getSelectedItem().equals("Euros")){
-                if(toComboBox.getSelectedItem().equals("Dollars")){
-                    String result = String.format("%.2f", amountToConvertion * eurosToDollars);
-                    resultConverter.setText(txtConvert.getText() +" € (EUR) = " +result + " $ (USD)");
-                }else{
-                    resultConverter.setText(txtConvert.getText()+" € (EUR) = " +txtConvert.getText()+ " € (EUR)");
-                }
+        double amountToConvertion = 0;
+        if(!(USDtoEUR.getText().isEmpty())){
+            amountToConvertion = Double.parseDouble(USDtoEUR.getText());
+            if (Double.parseDouble(USDtoEUR.getText()) > 0){
+                String result = String.format("%.2f", amountToConvertion * dollarsToEuros);
+                resultConverter.setText(USDtoEUR.getText() +" $ (USD) = " +result + " € (EUR)");
             }else{
-                if(toComboBox.getSelectedItem().equals("Euros")){
-                    String result = String.format("%.2f", amountToConvertion * dollarsToEuros);
-                    resultConverter.setText(txtConvert.getText() +" $ (USD) = " +result + " € (EUR)");
-                }else{
-                    resultConverter.setText(txtConvert.getText()+" $ (USD) = " +txtConvert.getText()+ " $ (USD)");
-                }
+                resultConverter.setText("No se admiten valores negativos, prueba nuevamente");
             }
-        }else{
-            resultConverter.setText("No se admiten valores negativos, prueba nuevamente");
+        }
+            
+        if(!(EURtoUSD.getText().isEmpty())){
+            amountToConvertion = Double.parseDouble(EURtoUSD.getText());
+            if (Double.parseDouble(EURtoUSD.getText()) > 0){
+                String result = String.format("%.2f", amountToConvertion * eurosToDollars);
+                resultConverter.setText(EURtoUSD.getText() +" € (EUR) = " +result + " $ (USD)");
+            }else{
+                resultConverter.setText("No se admiten valores negativos, prueba nuevamente");
+            }
         }
     }//GEN-LAST:event_convertButtonActionPerformed
-
-    private void toComboBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_toComboBoxActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_toComboBoxActionPerformed
-
-    private void fromComboBoxItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_fromComboBoxItemStateChanged
-
-    }//GEN-LAST:event_fromComboBoxItemStateChanged
 
     private void exitButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_exitButtonActionPerformed
         System.exit(0);
     }//GEN-LAST:event_exitButtonActionPerformed
 
-    private void txtConvertActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtConvertActionPerformed
+    private void USDtoEURActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_USDtoEURActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_txtConvertActionPerformed
+    }//GEN-LAST:event_USDtoEURActionPerformed
+
+    private void EURtoUSDActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_EURtoUSDActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_EURtoUSDActionPerformed
 
     /**
      * @param args the command line arguments
@@ -275,17 +229,15 @@ public class CurrencyConverter extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField EURtoUSD;
+    private javax.swing.JTextField USDtoEUR;
     private javax.swing.JButton convertButton;
     private javax.swing.JLabel euroDollarConversion;
     private javax.swing.JButton exitButton;
-    private javax.swing.JComboBox<String> fromComboBox;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel resultConverter;
-    private javax.swing.JComboBox<String> toComboBox;
-    private javax.swing.JTextField txtConvert;
     // End of variables declaration//GEN-END:variables
 }
